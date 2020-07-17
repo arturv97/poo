5881070e6e0515ecfeff98fb32cdd6eca3ebb464
arturv97 <68280271+arturv97@users.noreply.github.com>
2020-07-16 21:04:41 -0300

    Terceiro commit

--- Program.cs
+++ Program.cs
@@ -6,14 +6,43 @@ namespace poo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!\r\n");
+            Console.WriteLine("Hello World!");
             Console.ReadKey();
 
-            Pessoa objPessoa = new Pessoa();
-            objPessoa.nome = "Artur";
+            Pessoa objPessoa    = new Pessoa();
+            objPessoa.nome      = "NAME";
 
             Console.WriteLine(objPessoa.nome);
             Console.ReadKey();
         }
+        class Alunos
+        {
+            private void student()
+            {
+                Console.WriteLine("Alunos:\r\n");
+                Console.ReadKey();
+
+                Pessoa objAluno     = new Pessoa();
+                objAluno.nome       = "Artur";
+
+                Console.WriteLine(objAluno.nome);
+                Console.ReadKey();
+            }
+        }
+        class Professor
+        {
+            private void teacher()
+            {
+                Console.WriteLine("Professores:\r\n");
+                Console.ReadKey();
+
+                Pessoa objProfessor = new Pessoa();
+                objProfessor.nome   = "Mr. Samuels";
+
+                Console.WriteLine(objProfessor.nome);
+                Console.ReadKey();
+            }
+        }
     }
+    
 }
